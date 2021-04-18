a7c83d6af313fa6157b9b6572fe136c47d9550a2
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-04-18 19:57:17 +0700

    62130500092 [Add] components, image

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="buttonClick" class="btn" :class="[bgcolor, txtcolor]">
+    {{ label }}
+  </button>
+</template>
+<script>
+export default {
+  props: ['bgcolor', 'txtcolor', 'label'],
+  emits: ['butt-click'],
+  methods: {
+    buttonClick() {
+      this.$emit('butt-click', this.label)
+    }
+  }
+}
+</script>
